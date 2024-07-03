bbfe5cd70536cbfe0b6100851a5a9280776f8bfe
Du-devBR <eduardo.ananiasdev@outlook.com>
2024-07-02 21:19:33 -0300

    feat: Criar configurações nextjs-14 junto ao styled-components

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import { Teste } from "./style";
+
+export default function Home() {
+  return (
+    <div>
+      <Teste>Hello world!!</Teste>
+    </div>
+  );
+}
---
+++ src/app/style.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { themes } from "@/styles/global-themes";
+import styled from "styled-components";
+
+export const Teste = styled.h1`
+  color: ${themes.colors.$teste};
+`;
