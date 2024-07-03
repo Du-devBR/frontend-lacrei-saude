6dee892dea27e1603074628007162a839f9907be
Du-devBR <eduardo.ananiasdev@outlook.com>
2024-07-03 12:40:36 -0300

    style: Configura styled guide de cores, tipografias, grid

--- src/styles/global-themes.tsx
+++ src/styles/global-themes.tsx
@@ -1,5 +1,140 @@
 export const themes = {
   colors: {
-    $teste: "#018383",
+    brand: {
+      "$brand-emerald-60": "#018762",
+      "$brand-emerald-40": "#4FB494",
+      "$brand-emerald-20": "#B2DFD0",
+      "$brand-purple-60": "#7333B2",
+      "$brand-purple-40": "#A674D7",
+      "$brand-purple-20": "#D9C4EE",
+    },
+    text: {
+      "$text-heading": "#131313",
+      "$text-body": "#2D2D2D",
+      "$text-helper": "#404040",
+      "$text-accent": "#018762",
+      "$text-info": "#4D8ACB",
+      "$text-invert": "#fff",
+      "$text-hover": "#007756",
+      "$border-focus": "#4D8ACB",
+    },
+    background: {
+      "$background-default": "#FFF",
+      "$background-highlight": "#F9F3FF",
+      "$background-accent-lighter": "#DFF2ED",
+      "$background-accent-light": "#B2DFD0",
+      "$background-accent-medium": "4FB494",
+    },
+    gradient: {
+      "$gradient-lacrei": ["#018383", "#014687"],
+      "$gradient-header": ["#F5FFFB ", "#FFFFFF"],
+      "$gradient-simbolo": ["#008392", "#00BC86"],
+      "$gradient-rosa": ["#DF1098", "#BA17E2"],
+    },
+  },
+  typography: {
+    headline: {
+      "$Headline-xl": {
+        fontSize: "48px",
+        lineHeiht: "120%",
+        fontWeight: "700",
+      },
+      "$Headline-lg": {
+        fontSize: "40px",
+        lineHeiht: "120%",
+        fontWeight: "700",
+      },
+      "$Headline-base": {
+        fontSize: "32px",
+        lineHeiht: "120%",
+        fontWeight: "700",
+      },
+      "$Headline-sm": {
+        fontSize: "24px",
+        lineHeiht: "150%",
+        fontWeight: "300",
+      },
+      "$Headline-sm-highlight": {
+        fontSize: "24px",
+        lineHeiht: "150%",
+        fontWeight: "700",
+      },
+    },
+    text: {
+      "$Text-xl-highlight": {
+        fontSize: "18px",
+        lineHeiht: "150%",
+        fontWeight: "700",
+      },
+      "$Text-xl": {
+        fontSize: "18px",
+        lineHeiht: "150%",
+        fontWeight: "300",
+      },
+      "$Text-base": {
+        fontSize: "16px",
+        lineHeiht: "150%",
+        fontWeight: "300",
+      },
+      "$Text-base-highlight": {
+        fontSize: "16px",
+        lineHeiht: "150%",
+        fontWeight: "700",
+      },
+      "$Text-sm": {
+        fontSize: "14px",
+        lineHeiht: "150%",
+        fontWeight: "300",
+      },
+      "$Text-xs": {
+        fontSize: "12px",
+        lineHeiht: "150%",
+        fontWeight: "300",
+      },
+      "$Text-xs-highlight": {
+        fontSize: "16px",
+        lineHeiht: "150%",
+        fontWeight: "700",
+      },
+      "$Text-sm-highlight": {
+        fontSize: "14px",
+        lineHeiht: "150%",
+        fontWeight: "500",
+      },
+    },
+  },
+  grid: {
+    sm: {
+      columns: 4,
+      margin: "24px",
+      gutter: "16px",
+      rowHeight: "8",
+      maxWidth: "100%",
+      breakpoints: "600px",
+    },
+    md: {
+      columns: 8,
+      margin: "24px",
+      gutter: "24px",
+      rowHeight: "8",
+      maxWidth: "100%",
+      breakpoints: "900px",
+    },
+    lg: {
+      columns: 12,
+      margin: "24px",
+      gutter: "16px",
+      rowHeight: "8",
+      maxWidth: "840px",
+      breakpoints: "1200px",
+    },
+    xl: {
+      columns: 12,
+      margin: "24px",
+      gutter: "16px",
+      rowHeight: "8",
+      maxWidth: "1116px",
+      breakpoints: "1536px",
+    },
   },
 };
